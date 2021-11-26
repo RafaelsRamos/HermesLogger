af706fdd419c9663433f59fe9af48214a3894492
Rafael Ramos <rafaelsramos95@gmail.com>
2021-11-26 22:11:56 +0000

    simplify file

--- Hermes/Logger/src/main/java/com/spartancookie/hermeslogger/ui/fragments/InfoDetailedViewFragment.kt
+++ Hermes/Logger/src/main/java/com/spartancookie/hermeslogger/ui/fragments/InfoDetailedViewFragment.kt
@@ -4,39 +4,27 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
-import android.view.View.VISIBLE
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.spartancookie.formatter.Formatter
 import com.spartancookie.hermeslogger.R
-import com.spartancookie.hermeslogger.debugToaster.LogType
 import com.spartancookie.hermeslogger.models.LogDataHolder
 import com.spartancookie.hermeslogger.ui.setCreationDate
 import com.spartancookie.hermeslogger.ui.setLogIcon
 import com.spartancookie.hermeslogger.utils.*
-import com.spartancookie.hermeslogger.utils.animateCopyToClipboardColor
-import com.spartancookie.hermeslogger.utils.copyToClipboard
-import com.spartancookie.hermeslogger.utils.removeFromStack
-import com.spartancookie.hermeslogger.utils.shareLog
+import kotlinx.android.synthetic.main.screen_detailed_view.*
 
 private const val ITEM_ARG = "selected_item"
 
 internal class InfoDetailedViewFragment : Fragment(R.layout.screen_detailed_view) {
 
-    private val backIV by lazy { rootLayout.findViewById<ImageView>(R.id.back) }
-    private val copyToClipboardIV by lazy { rootLayout.findViewById<ImageView>(R.id.copy_to_clipboard) }
-    private val shareLogIV by lazy { rootLayout.findViewById<ImageView>(R.id.share_icon) }
-    private val typeIconIV by lazy { rootLayout.findViewById<ImageView>(R.id.type_icon) }
-    private val typeNameTV by lazy { rootLayout.findViewById<TextView>(R.id.typeName) }
-    private val dateTV by lazy { rootLayout.findViewById<TextView>(R.id.date) }
-    private val messageTV by lazy { rootLayout.findViewById<TextView>(R.id.message) }
-    private val extraInfoTV by lazy { rootLayout.findViewById<TextView>(R.id.extra_info) }
-    private val genericInfoTV by lazy { rootLayout.findViewById<TextView>(R.id.generic_info) }
-
     private lateinit var rootLayout: View
-
     private lateinit var item: LogDataHolder
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        arguments?.getParcelable<LogDataHolder>(ITEM_ARG)?.let {
+            item = it
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
@@ -47,13 +35,19 @@ internal class InfoDetailedViewFragment : Fragment(R.layout.screen_detailed_view
         rootLayout = view
 
         // Set listeners
-        backIV.setOnClickListener { removeFromStack(parentFragmentManager, TAG) }
-        copyToClipboardIV.setOnClickListener { imageView ->
+        //findView<View>(R.id.back)
+        back.setOnClickListener {
+            removeFromStack(parentFragmentManager, TAG)
+        }
+
+        //findView<View>(R.id.copy_to_clipboard)
+        copy_to_clipboard.setOnClickListener { imageView ->
             animateCopyToClipboardColor(imageView)
             activity?.let { copyToClipboard(it, item) }
         }
 
-        shareLogIV.run {
+        //findView<View>(R.id.share_icon)
+        share_icon.run {
             if (hasWriteStoragePermission(context)) {
                 setOnClickListener { context?.run { shareLog(this, item) } }
             } else {
@@ -61,22 +55,17 @@ internal class InfoDetailedViewFragment : Fragment(R.layout.screen_detailed_view
             }
         }
 
-        // Fill views with log data
-        typeIconIV.setLogIcon(item.type)
-        dateTV.setCreationDate(item)
-        typeNameTV.text = item.type.name
-        messageTV.text = item.message
-        genericInfoTV.text = item.genericInfo
+        type_icon.setLogIcon(item.type)
+
+        typeName.text = item.type.name
+
+        date.setCreationDate(item)
+
+        message.text = item.message
+        generic_info.text = item.genericInfo
 
         item.extraInfo?.let { extraInfo ->
-            extraInfoTV.text = item.dataType?.let { Formatter.format(it, extraInfo) } ?: extraInfo
-        }
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        arguments?.getParcelable<LogDataHolder>(ITEM_ARG)?.let {
-            item = it
+            extra_info.text = item.dataType?.format(extraInfo) ?: extraInfo
         }
     }
 
